a903fe82662493cf177ac5bd539d930caaed352e
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-03-04 15:29:49 -0500

    chore(actions): test

--- tasks/release-notifications.js
+++ tasks/release-notifications.js
@@ -30,7 +30,7 @@ const githubToken = args.token || process.env.GITHUB_TOKEN;
  * Release version (-v)
  * @type {string}
  */
-const releaseVersion = args.version || process.env.VERSION;
+const releaseVersion = args.version;
 
 /**
  * Github Repo Slug
